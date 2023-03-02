7734c68820fa79fe2bdfe36828dc1e5e7d010f77
BryanGIG <contacts.bryangig@gmail.com>
2023-03-02 13:12:31 +0700

    Add setup Storage permission on start up

--- app/src/main/java/com/dumper/android/core/MainActivity.kt
+++ app/src/main/java/com/dumper/android/core/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.dumper.android.core
 
+import android.Manifest
 import android.content.Intent
 import android.content.Intent.ACTION_VIEW
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,12 +12,19 @@ import android.os.Message
 import android.os.Messenger
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.anggrayudi.storage.SimpleStorageHelper
+import com.anggrayudi.storage.file.StorageId
+import com.anggrayudi.storage.permission.ActivityPermissionRequest
+import com.anggrayudi.storage.permission.PermissionCallback
+import com.anggrayudi.storage.permission.PermissionReport
+import com.anggrayudi.storage.permission.PermissionResult
 import com.dumper.android.R
 import com.dumper.android.core.RootServices.Companion.IS_FIX_NAME
 import com.dumper.android.core.RootServices.Companion.IS_FLAG_CHECK
@@ -36,6 +45,7 @@ import com.dumper.android.ui.memory.MemoryFragment
 import com.dumper.android.ui.memory.MemoryViewModel
 import com.topjohnwu.superuser.ipc.RootService
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
@@ -46,6 +56,29 @@ class MainActivity : AppCompatActivity() {
     val memory: MemoryViewModel by viewModels()
     val console: ConsoleViewModel by viewModels()
 
+    private val permissionRequest = ActivityPermissionRequest.Builder(this)
+        .withPermissions(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        .withCallback(object : PermissionCallback {
+            override fun onPermissionsChecked(result: PermissionResult, fromSystemDialog: Boolean) {
+                val grantStatus = if (result.areAllPermissionsGranted) "granted" else "denied"
+                Toast.makeText(
+                    baseContext,
+                    "Storage permissions are $grantStatus",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            override fun onShouldRedirectToSystemSettings(blockedPermissions: List<PermissionReport>) {
+                SimpleStorageHelper.redirectToSystemSettings(this@MainActivity)
+            }
+        })
+        .build()
+
+    private val storageHelper = SimpleStorageHelper(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -55,27 +88,10 @@ class MainActivity : AppCompatActivity() {
         Fixer.extractLibs(this)
 
         if (intent.getBooleanExtra("IS_ROOT", false)) {
-            initRootService()
-        }
-    }
-
-    private fun setNavigationController() {
-
-        val navController =
-            binding.navHostFragmentActivityMain.getFragment<NavHostFragment>().navController
-
-        val appBarConfiguration =
-            AppBarConfiguration(setOf(R.id.nav_memory_fragment, R.id.nav_console_fragment))
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.navView.setupWithNavController(navController)
-    }
-
-    private fun initRootService() {
-        if (remoteMessenger == null) {
-            dumperConnection = MSGConnection(this)
-            val intent = Intent(applicationContext, RootServices::class.java)
-            RootService.bind(intent, dumperConnection)
+            setupRootService()
+        } else {
+            setupSimpleStorage(savedInstanceState)
+            setupStoragePermission()
         }
     }
 
@@ -137,6 +153,42 @@ class MainActivity : AppCompatActivity() {
 
                 )
             }
+        }
+    }
+
+    private fun setupSimpleStorage(savedInstanceState: Bundle?) {
+        savedInstanceState?.let { storageHelper.onRestoreInstanceState(it) }
+    }
+
+    private fun setupStoragePermission() {
+        if (Build.VERSION.SDK_INT in 23..28) {
+            permissionRequest.check()
+        } else if (Build.VERSION.SDK_INT == 29){
+            storageHelper.requestStorageAccess()
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!storageHelper.storage.isStorageAccessGranted(StorageId.PRIMARY)) {
+                storageHelper.storage.requestFullStorageAccess()
+            }
+        }
+    }
+
+    private fun setNavigationController() {
+
+        val navController =
+            binding.navHostFragmentActivityMain.getFragment<NavHostFragment>().navController
+
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.nav_memory_fragment, R.id.nav_console_fragment))
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    private fun setupRootService() {
+        if (remoteMessenger == null) {
+            dumperConnection = MSGConnection(this)
+            val intent = Intent(applicationContext, RootServices::class.java)
+            RootService.bind(intent, dumperConnection)
         }
     }
 
