064a426f7a72fc19b098a0385ecd081564932c23
BryanGIG <contacts.bryangig@gmail.com>
2024-01-09 16:48:22 +0700

    Update: Add warning for KernelSU user

--- app/src/main/java/com/dumper/android/core/SplashActivity.kt
+++ app/src/main/java/com/dumper/android/core/SplashActivity.kt
@@ -5,8 +5,12 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.content.edit
 import com.dumper.android.BuildConfig
+import com.dumper.android.ui.config.IGNORE_KSU
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.topjohnwu.superuser.Shell
+import kotlin.system.exitProcess
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : Activity() {
@@ -23,16 +27,69 @@ class SplashActivity : Activity() {
         }
     }
 
+    private val sharedPreferences by lazy {
+        getSharedPreferences("settings", MODE_PRIVATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Shell.getShell {
-            val methodStr = if (it.isRoot) "root" else "non-root"
-            Toast.makeText(this@SplashActivity, "Using $methodStr method", Toast.LENGTH_SHORT).show()
 
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
-            intent.putExtra("IS_ROOT", it.isRoot)
+        kotlin.runCatching {
+            val launcher = packageManager.getLaunchIntentForPackage("me.weishu.kernelsu")
+            if (launcher != null) {
+                requestKSUPermission(launcher)
+                return
+            }
+        }
 
-            startActivity(intent)
+        initShell()
+    }
+
+    private fun requestKSUPermission(launcher: Intent) {
+        sharedPreferences.getBoolean(IGNORE_KSU, false).also {
+            if (it) {
+                initShell()
+                return
+            }
+        }
+
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Warning")
+            .setMessage(
+                "KernelSU is detected, please grant root permission manually!\n\n" +
+                        "If you want to use Non Root method, ignore this 'warning and continue."
+            )
+            .setPositiveButton("Launch") { _, _ ->
+                startActivity(launcher)
+                exitProcess(0)
+            }
+            .setNegativeButton("Ignore") { _, _ ->
+                sharedPreferences.edit(commit = true) {
+                    putBoolean(IGNORE_KSU, true)
+                }
+                initShell()
+            }
+            .setOnCancelListener {
+                sharedPreferences.edit(commit = true) {
+                    putBoolean(IGNORE_KSU, true)
+                }
+                initShell()
+            }
+            .show()
+    }
+
+    private fun initShell() {
+        Shell.getShell { shell ->
+            val methodStr = if (shell.isRoot) "root" else "non-root"
+            Toast.makeText(applicationContext, "Using $methodStr method", Toast.LENGTH_SHORT)
+                .show()
+            launchActivity()
+        }
+    }
+
+    private fun launchActivity() {
+        Intent(applicationContext, MainActivity::class.java).also {
+            startActivity(it)
             finish()
         }
     }
