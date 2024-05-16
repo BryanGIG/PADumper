7cad632c6ecd4e25f79a39ac9f254de43d7d6c6b
BryanGIG <contacts.bryangig@gmail.com>
2024-05-16 09:40:13 +0700

    Console: Make output selectable

--- app/src/main/java/com/dumper/android/ui/console/ConsoleScreen.kt
+++ app/src/main/java/com/dumper/android/ui/console/ConsoleScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ContentCopy
@@ -56,13 +57,15 @@ fun ConsoleScreen(navController: NavController, viewModel: ConsoleViewModel = vi
                 .horizontalScroll(horizontalState)
                 .padding(10.dp)
         ) {
-            Text(
-                text = console,
-                fontSize = 12.sp,
-                fontFamily = FontFamily.Monospace,
-                modifier = Modifier
-                    .padding(8.dp)
-            )
+            SelectionContainer {
+                Text(
+                    text = console,
+                    fontSize = 12.sp,
+                    fontFamily = FontFamily.Monospace,
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+            }
         }
 
         FloatingActionButton(
